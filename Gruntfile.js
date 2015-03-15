ff271cdc7deecd87dce65f13711bf97e0fc45f67
Ben Alman <cowboy@rj3.net>
2015-03-15 09:01:05 -0400

    Generate inline sourcemaps (less files to commit).

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     babel: {
       options: {
-        sourceMap: true,
+        sourceMap: 'inline',
         experimental: true,
         optional: ['runtime'],
       },
